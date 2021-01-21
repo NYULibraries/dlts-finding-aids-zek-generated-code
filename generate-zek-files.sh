c66d6748b3961c5358e9ed38872dee1e21def088
David <da70@nyu.edu>
2021-01-21 14:30:13 -0500

    generate-zek-files.sh: recreate sample-set-1-individual-ead-files/ files

--- generate-zek-files.sh
+++ generate-zek-files.sh
@@ -10,5 +10,9 @@ git clone git@github.com:NYULibraries/dlts-finding-aids-ead-sample-set-1.git
 
 find dlts-finding-aids-ead-sample-set-1/ -type f -name *.xml -exec cat {} \; | zek | grep -v '// Ead was generated' > sample-set-1-ead-files.go
 
+rm sample-set-1-individual-ead-files/*
+
+for eadfile in $( find dlts-finding-aids-ead-sample-set-1/ -type f -name *.xml ); do eadid=$( basename $eadfile .xml ); cat $eadfile | zek | grep -v '// Ead was generated' > sample-set-1-individual-ead-files/${eadid}.go; done
+
 rm -fr dlts-finding-aids-ead-sample-set-1/
 
